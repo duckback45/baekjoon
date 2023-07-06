24ff42a80fd7dd8e5e01560910b062dd98a8709b
duckback45 <duckback45@gmail.com>
2023-07-07 00:27:31 +0900

    20230707

---
+++ Bronze4/2439.py
@@ -0,0 +1,4 @@
+# n  = int(input())
+n = 5
+for i in range(1, n+1):
+    print(' ' * (n - i) + ('*' * i))
--- greedy/10162.py
+++ greedy/10162.py
@@ -1,1 +1,12 @@
 #전자레인지
+T = int(input())
+time = [300, 60, 10]
+result = []
+for i in time:
+    result.append(T//i)
+    T = T%i
+print(T)
+if T == 0:
+    print(*result)
+else:
+    print(-1)
